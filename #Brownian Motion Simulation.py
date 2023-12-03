1dcdc2e5f7f3be5d07d3e33c8dbd88c68222e765
EthanFeld <145927260+EthanFeld@users.noreply.github.com>
2023-12-02 22:39:37 -0500

    Add files via upload

--- #Brownian Motion Simulation.py
+++ #Brownian Motion Simulation.py
@@ -10,12 +10,21 @@ distance = lambda p1, p2: ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) **0.5
 class ball:
     #initialize
     def  __init__(self, vx, vy, m, r, posX, posY):
-        self.v = [vx, vy]
+        self.v = [vx * 10, vy * 10]
         self.m = m
-        self.r = r
+        self.r = r*1.1
         self.pos = [posX, posY]
+        self.color = "red"
     #collide
     def collide(self, ball2):
+        dist = distance(ball2.pos, self.pos)
+        dawg = ball2.r + self.r - dist 
+        if(dawg > 0):
+            rt2 = 2 **0.5
+            addy =  dawg/rt2
+            max(ball2, self, key = lambda a: a.pos[0]).pos[0] += addy 
+            max(ball2, self, key = lambda a: a.pos[1]).pos[1] += addy
+        dist = distance(ball2.pos, self.pos)  
         #variables
         vx1, vy1 = self.v
         x1,y1 = self.pos
@@ -26,7 +35,7 @@ class ball:
         dy = y1 - y2
         dvx = vx1 - vx2
         dvy = vy1 - vy2
-        dist = max(distance(ball2.pos, self.pos), 0.00001)
+        
         cosTheta = (dy)/dist
         sinTheta = (dx)/dist
         vx1n = vx1 + sinTheta*( -vx1*sinTheta + vx2*sinTheta - vy1*cosTheta + vy2*cosTheta)
@@ -37,16 +46,24 @@ class ball:
         self.v = [vx1n, vy1n]
     def wallBounce(self):
         self.v[0] = -1 * self.v[0] 
+        if(self.pos[0] - self.r < 0):
+            self.pos[0] = self.r
+        else:
+            self.pos[0] = 1280 - self.r
     def ceilingBounce(self):
         self.v[1] = -1 * self.v[1]
+        if(self.pos[1] - self.r <= 0):
+            self.pos[1] = self.r
+        else:
+            self.pos[1] = 720 - self.r
     def kineticEnergy(self):
-        return 0.5*m*v**2
+        return 0.5*self.m*(self.v[0]**2 + self.v[1]**2)
 class ensemble:
     balls = []
     def __init__(self):
         self.balls = []
     def findTemp(self):
-        return kb * 1.5 * [i.kineticEnergy(i) for i in self.balls]
+        return kb * 1.5 * sum([i.kineticEnergy() for i in self.balls])/len(self.balls)
     def addBalls(self, count):
         for i in range(count):
             antonio = ball(random.randint(-10,10),random.randint(-10,10),10,10, random.randint(200,1200), random.randint(200,400))
@@ -54,16 +71,15 @@ class ensemble:
     def collisions(self):
         alrDone = set()
         for i in self.balls:
-            
             for j in self.balls:
                 if i is not j and (j, i) not in alrDone:
                     if distance(i.pos, j.pos) <= i.r + j.r:
                         i.collide(j)
                 alrDone.add((i,j))
         for i in self.balls:
-            if(abs((i.pos[0] +i.r)- 640) >= 640):
+            if(abs((i.pos[0] +i.r)- 640) >= 640 or i.pos[0] <=i.r):
                 i.wallBounce()
-            if(abs((i.pos[1] +i.r)- 360) >= 360):
+            if(abs((i.pos[1] +i.r)- 360) >= 360 or i.pos[1] <=i.r):
                 i.ceilingBounce()
     def drawDawg(self):
         rtn =[]
@@ -81,7 +97,12 @@ running = True
 joe = []
 Group = ensemble()
 Group.addBalls(200)
+points = []
+cnt = 0
 while running:
+    myFont = pygame.font.SysFont("Times New Roman", 18)
+    Disp = myFont.render(str(Group.findTemp()), True, 1)
+    cnt += 1
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -91,14 +112,22 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
     #handle collisons
+    
     Group.collisions()
     Group.advance()
+    atro = 0
     for i in Group.drawDawg():
         #print(i)
-        pygame.draw.circle(screen, "red", i[:2], i[2])
-    
-    
-
+        atro += 1
+        if(atro == 1):
+            pygame.draw.circle(screen, "red", i[:2], i[2])
+        else:
+            pygame.draw.circle(screen, "blue", i[:2], i[2])
+    if(len(points) > 2):
+        pygame.draw.lines(screen,"green", False,points, 15)
+    if(cnt > 100):
+        points.append(Group.balls[50].pos)
+    screen.blit(Disp,(520,30))
     # flip() the display to put your work on screen
     pygame.display.flip()
 
